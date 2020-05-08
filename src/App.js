f4b3b38bee50871882dcd6a00d4dba50a683a4cc
emmydame <emygreat1912@yahoo.com>
2020-05-08 09:32:17 +0100

    Environment Setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { hot} from 'react-hot-loader';
+
+const App = ()=>(
+    <div className="App">
+        <hi>Hello Emilia !!!!!!</hi>
+    </div>
+);
+
+export default hot(module) (App);
